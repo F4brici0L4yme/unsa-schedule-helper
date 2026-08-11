c3ac9b2d61c8404399a11fc42b52675247630c63
F4bricioL4yme <rlaymes@unsa.edu.pe>
2026-08-10 20:45:14 -0500

    feat: add script for table borders and content detection

--- script-tests/pdfplumber-test.py
+++ script-tests/pdfplumber-test.py
@@ -1,7 +1,15 @@
 import pdfplumber
 
 with pdfplumber.open("../pdf/teoria-2026b.pdf") as pdf:
-    first_page = pdf.pages[0]
-    im = first_page.to_image()
-    im.draw_rects(first_page.extract_words())
-    im.save("word_detection.png")
+    page = pdf.pages[0]
+    table = page.extract_table()
+    im = page.to_image()
+    debugged = im.debug_tablefinder()
+    debugged.save("../export/imgs/table_detection.png")
+#    first_page = pdf.pages[0]
+#    im = first_page.to_image()
+#    im.draw_rects(first_page.extract_words())
+#    im.save("word_detection.png")
+
+
+
