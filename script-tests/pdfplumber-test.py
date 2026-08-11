45fdbf8d25552d12b522c74962521e6e6bf950cf
F4bricioL4yme <rlaymes@unsa.edu.pe>
2026-08-10 19:19:05 -0500

    feat: add script for word detection

--- script-tests/pdfplumber-test.py
+++ script-tests/pdfplumber-test.py
@@ -2,4 +2,6 @@ import pdfplumber
 
 with pdfplumber.open("../pdf/teoria-2026b.pdf") as pdf:
     first_page = pdf.pages[0]
-    print(first_page.chars[0])
+    im = first_page.to_image()
+    im.draw_rects(first_page.extract_words())
+    im.save("word_detection.png")
